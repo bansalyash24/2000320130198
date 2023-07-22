f79f2017996c441b5b8a75d238cbce3fb59f4b7d
bansalyash24 <bansalyash066@gmail.com>
2023-07-22 13:16:48 +0530

    update

--- backend/routes/singleTrain.js
+++ backend/routes/singleTrain.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const {getToken}=require('../helpers/helper')
 
 router.get('/train/trains/:id',async function getTrain(req,res){
-    console.log(req.params.id)
     try {
         const token=await getToken()
         console.log('token',token)
-        const data=fetch(`http://20.244.56.144/train/trains/${req.params.id}`,{
+        const data=await fetch(`http://20.244.56.144/train/trains/${req.params.id}`,{
             method:'GET',
             headers: { Authorization: `Bearer ${token.access_token}` }
         })
